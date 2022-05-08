81bbf0f5bbde36de5b76611c4b85e92a948827c9
Luis Fernando Illapa <moxb@outlook.es>
2022-05-07 20:44:17 -0500

    refactor(back): remove unused dependencies

--- back/src/twilio/twilio.service.ts
+++ back/src/twilio/twilio.service.ts
@@ -1,6 +1,5 @@
 import { ConfigService } from '@nestjs/config';
 import { jwt, Twilio } from 'twilio';
-import { v4 as uuidv4 } from 'uuid';
 
 export class TwilioService {
   public twilioClient: Twilio;
